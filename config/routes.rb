822934a98de4f1780a913cfa03b2a25d951e0785
이혜민 <ihyemin@ihyemin-ui-MacBook-Pro.local>
2018-07-03 02:35:14 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  
+  root 'home#index'
+  get 'home/index'
+
+  get 'home/new'
+
+  post 'home/create' => 'home#create', as: 'posts'
+  
+  get 'home/destroy/:post_id' => 'home#destroy', as: 'post_destroy'
+  #get 'home/destroy/:post_id', to: 'home#destroy', as: 'post_destroy'
+
+  get 'home/edit/:post_id' => 'home#edit'
+  
+  patch 'home/update/:post_id' => 'home#update', as: 'post'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+ 
